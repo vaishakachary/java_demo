539dd0bb31a44f79bbbb18a4045986adefc6ca47
vaishakachary <159225703+vaishakachary@users.noreply.github.com>
2024-02-13 20:06:46 -0800

    Add files via upload

---
+++ findhighest.java
@@ -0,0 +1,22 @@
+import java.util.scanner;
+class findhighest
+{
+  public static void main(String args[])
+  {
+   int marks[]=new int[10];
+   int number;
+   Scanner s= new Scanner(System.in);
+   int heighest=0;
+   system.out.println("enter the marks of 10 students");
+   for(int i=0;i<10;i++)
+    {
+     system.out.println("enter tho marks of student number"+i+1);
+	 number=s.nextint()
+	 marks[i=number]
+	 for(i=0;i<10;i++)
+	 if marks(i>heighest)
+	 heighest=marks(i)
+	 system.out.println("heighest marks is="+heighest)
+	 }
+	}
+}
